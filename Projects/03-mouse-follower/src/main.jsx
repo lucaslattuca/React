c8ef18a399ee52b97ae186f021dba1ac643bbbb5
Lucas Lattuca <38288874+lucaslattuca@users.noreply.github.com>
2023-10-23 16:04:29 -0300

    comment in StrictMode

--- Projects/03-mouse-follower/src/main.jsx
+++ Projects/03-mouse-follower/src/main.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //el strictMode te ayuda a depurar, en produccion lo ignora
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
