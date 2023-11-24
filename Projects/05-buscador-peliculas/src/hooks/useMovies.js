c7b15889ff0d28e7d29558ceff185efe5287152e
Lucas Lattuca <38288874+lucaslattuca@users.noreply.github.com>
2023-11-24 12:07:05 -0300

    evitar buscar lo que ya se buscó

--- Projects/05-buscador-peliculas/src/hooks/useMovies.js
+++ Projects/05-buscador-peliculas/src/hooks/useMovies.js
@@ -1,15 +1,18 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({search}) {
     const [movies, setMovies] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, SetError] = useState(null)
+    const previusSearch = useRef(search)
 
     const getMovies = async () => {
+      if (search === previusSearch.current) return // con esto evitamos que haga la busqueda de algo que ya buscó
       try {
         setLoading(true)
         SetError(null)
+        previusSearch.current = search
         const moviesResponse = await searchMovies({ search })
         setMovies(moviesResponse)
       } catch (e) {
