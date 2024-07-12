d4c35f0bbe6bb2990843b3be092104593a09b294
Lucas Lattuca <38288874+lucaslattuca@users.noreply.github.com>
2024-07-11 21:57:26 -0300

    Update facts.js

--- Projects/04-prueba-tecnica/src/services/facts.js
+++ Projects/04-prueba-tecnica/src/services/facts.js
@@ -1,9 +1,21 @@
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
 //por lo gral en las entrevistas te prohiben usar react Query, axios, apollo, SWR y tienes que usar fetch
-export const getRandomFact = async () => {
+
+// con promesas
+export const getRandomFact = () => {
+    return fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => res.json())
+        .then(data => {
+            const { fact } = data
+            return fact
+        })
+}
+
+//con async-await
+/*export const getRandomFact = async () => {
     const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
     const data = await res.json()
     const { fact } = data
     return fact
-}
+}*/
